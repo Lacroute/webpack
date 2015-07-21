c1411d9e78d90d60d8f56bdd115a127c8d274e1d
Pablo SINIVASSIN <pablo.sinivassin@afp.com>
2015-07-21 12:47:25 +0200

    301 : Problème d'installation de librairie gulp-html-prettify : removing the lib from workflow

--- Gulp/tasks/html.js
+++ Gulp/tasks/html.js
@@ -3,7 +3,7 @@ var gutil  = require('gulp-util');
 var newer  = require('gulp-newer');
 var path = require('path');
 var jade = require('gulp-jade');
-var html = require('gulp-html-prettify');
+// var html = require('gulp-html-prettify');
 var fs = require('fs');
 var data = JSON.parse(fs.readFileSync('./src/html/graphics.json'));
 
@@ -19,8 +19,8 @@ gulp.task('jade', function() {
     .pipe(jade({
       locals: data.metadata
     }))
-    .pipe(html({
-      indent_char: ' ', indent_size: 2
-    }))
+    // .pipe(html({
+    //   indent_char: ' ', indent_size: 2
+    // }))
     .pipe(gulp.dest('dest/'));
 });
