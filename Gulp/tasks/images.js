1669b84f72d89af471653eed181f802a7e7e1a7e
Pablo SINIVASSIN <pablo.sinivassin@afp.com>
2015-08-17 17:51:10 +0200

    adding images optim tasks

--- Gulp/tasks/images.js
+++ Gulp/tasks/images.js
@@ -2,9 +2,32 @@ var gulp   = require('gulp');
 var gutil  = require('gulp-util');
 var bs     = require('browser-sync').create();
 var newer  = require('gulp-newer');
+var imagemin = require('imagemin');
+var svgo = require('imagemin-svgo');
+var jpg = require('imagemin-jpgtran');
+var optipng = require('imagemin-optipng');
 
-gulp.task('images', function(cb) {
-  return gulp.src('src/images/**/*.{png,jpg,svg}')
+
+
+gulp.task('png', function() {
+  return gulp.src('src/images/**/*.png')
+    .pipe(optipng({optimizationLevel: 3})())
     .pipe(newer('dest/images'))
     .pipe(gulp.dest('dest/images'));
 });
+
+gulp.task('jpg', function() {
+  return gulp.src('src/images/**/*.jpg')
+    .pipe(jpg({progressive: true})())
+    .pipe(newer('dest/images'))
+    .pipe(gulp.dest('dest/images'));
+});
+
+gulp.task('svg', function() {
+  return gulp.src('src/images/**/*.svg')
+    .pipe(svgo()())
+    .pipe(newer('dest/images'))
+    .pipe(gulp.dest('dest/images'));
+});
+
+gulp.task('images', ['png', 'jpg', 'svg']);
