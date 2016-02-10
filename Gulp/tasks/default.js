77eb93628d0f26816eec45df4ad36c05febcd092
Pablo SINIVASSIN <pablo.sinivassin@afp.com>
2016-02-10 19:03:30 +0100

    optim perfg

--- Gulp/tasks/default.js
+++ Gulp/tasks/default.js
@@ -19,13 +19,13 @@ gulp.task('default', ['changedDatas', 'others', 'fonts', 'sass', 'scripts', 'mod
 
   gulp.watch('./src/scripts/**/*.js', ['scripts', browserSync.reload]);
 
-  watch('./src/images/**/*.{png,jpg,svg}', ['images', browserSync.reload]);
+  gulp.watch('./src/images/**/*.{png,jpg,svg}', ['images', browserSync.reload]);
 
-  watch('./src/data/'+ dev_lang +'/**/*.{json,csv,tsv,txt}', ['changedDatas', browserSync.reload]);
+  gulp.watch('./src/data/'+ dev_lang +'/**/*.{json,csv,tsv,txt}', ['changedDatas', browserSync.reload]);
 
-  watch('./src/html/**/*.{json,txt,svg,ico}', ['others', browserSync.reload]);
+  gulp.watch('./src/html/**/*.{json,txt,svg,ico}', ['others', browserSync.reload]);
 
-  watch('./src/styles/fonts/'+ dev_lang +'/*.{eot,svg,ttf,woff,woff2}', ['fonts', browserSync.reload]);
+  gulp.watch('./src/styles/fonts/'+ dev_lang +'/*.{eot,svg,ttf,woff,woff2}', ['fonts', browserSync.reload]);
 
   if (typeof dev_lang === 'undefined') {
     gutil.log(gutil.colors.red('Veuillez indiquer la langue avec la commande "gulp --lang xx-XX"'));
