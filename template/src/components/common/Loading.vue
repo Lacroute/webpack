491dd25e5b7d22368b79f10fd991d8e09645e68c
Clement Procureur <clement@sgrx04.afp.local>
2016-11-28 16:58:46 +0100

    loading component

--- template/src/components/common/Loading.vue
+++ template/src/components/common/Loading.vue
@@ -1,26 +1,18 @@
 <template lang="html">
-  <div class="loading">
-    <div class="container">
-
-      <div class="locales" :data-status="localesStatus">
-        <div v-if="localesStatus">
-          <span>Locales {{ localesStatus }}</span>
-        </div>
-        <div v-else>
-          <span>Locales not required</span>
-        </div>
-      </div>
-
-      <div class="datas" :data-status="dbsStatus">
-        <div v-if="dbsStatus">
-          <span>DBs {{ dbsStatus }}</span>
-        </div>
-        <div v-else>
-          <span>DBs not required</span>
+  <transition name="fade">
+    <div class="loading" v-if="!isLoaded">
+      <div class="container">
+        <div class="content">
+          <div class="loader-animation">
+            <svg class="circular" viewBox="25 25 50 50">
+              <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+            </svg>
+          </div>
+          <h1>{{$t('Loading')}}</h1>
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -30,21 +22,96 @@ export default {
   computed: {
     ...mapGetters({
       localesStatus: 'localesStatus',
-      dbsStatus: 'dbsStatus'
-    }),
+      dbsStatus: 'dbsStatus',
+      isLoaded: 'isLoaded'
+    })
   },
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+@import '~utils/global';
+
 .loading{
-  position: absolute;
-  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: table;
+  background: $loading_bg;
+  color: $loading_color;
 }
+
 .container{
-  padding: 12px;
+  display: table-cell;
+  vertical-align: middle;
+
+  h1{
+    font-family: $source_sans_pro;
+    text-transform: uppercase;
+    font-size: 0.8em;
+  }
+}
+
+.loader-animation {
   position: relative;
-  z-index: 9999;
+  margin: 0 auto;
+  width: 32px;
+  &:before {
+    content: '';
+    display: block;
+    padding-top: 100%;
+  }
+}
+
+.circular {
+  animation: rotate 2s linear infinite;
+  height: 100%;
+  transform-origin: center center;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+
+.path {
+  stroke-dasharray: 1, 200;
+  stroke-dashoffset: 0;
+  animation: dash 1.5s ease-in-out infinite;
+  stroke-linecap: round;
+  stroke: $loading_color;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -35px;
+  }
+  100% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -124px;
+  }
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
 }
 .loading [data-status="LOADED"] { color: green }
 .loading [data-status="LOADING"] { color: gold }
--- template/src/store/getters.js
+++ template/src/store/getters.js
@@ -1,2 +1,5 @@
+import * as status from './status-types'
+
 export const localesStatus = state => state.locales.status
 export const dbsStatus     = state => state.dbs.status
+export const isLoaded      = state => (state.locales.status === status.LOADED && state.dbs.status === status.LOADED && true)
